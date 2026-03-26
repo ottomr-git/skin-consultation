878a2c0888b770eb901c227ece72771c9183aee1
ottomr-git <otto.mr@odelia-lab.com>
2026-03-26 23:21:43 +0800

    設定計數器 API 網址

--- counter.js
+++ counter.js
@@ -1,6 +1,6 @@
 // ===== 頁面計數器 =====
 // COUNTER_URL 部署 Google Apps Script 後填入
-var COUNTER_URL = '';
+var COUNTER_URL = 'https://script.google.com/macros/s/AKfycbxX6ktYYxO0Nvg_bbd6Af66nn88DmfpSIPNLLi1VY_KX2Myk-8A3Tm62f-hpQDMv3oIfg/exec';
 var PAGE_ID = 'skin-consultation';
 
 (function () {
